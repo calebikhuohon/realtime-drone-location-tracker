a9d3d4999004dbd62eb6cf74241091823fdc362d
Caleb Ikhuohon <calebikhuohon@gmail.com>
2020-01-16 12:45:21 +0100

    feat: define utility methods to be used by the drone

---
+++ src/utility.js
@@ -0,0 +1,16 @@
+/**
+ * take the drone location command as string and return data as an object
+ * @param {String} locationBuffer 
+ */
+function parseDroneLocation (locationBuffer) {
+    
+}
+
+function verifyCommand(command) {
+    return;
+}
+
+export {
+    parseDroneLocation,
+    verifyCommand
+}

63370a805c406416cd70dc4126edccddc1bb74cc
Caleb Ikhuohon <calebikhuohon@gmail.com>
2020-01-17 22:22:28 +0100

    refactor: update test with new data

--- backend/test/utility.spec.js
+++ backend/test/utility.spec.js
@@ -3,13 +3,13 @@ import assert from 'assert';
 
 describe('parseDroneLocation function', () => {
   it('parse drone location', () => {
-    let location = 'droneId:12345;location:51.507351,-0.127758;';
+    let data = 'speed:10;location:51.507351,-0.127758;';
     let expected = {
-      droneId: 12345,
+      speed: 10,
       location: [51.507351,-0.127758],
     };
 
-    let actual = parseDroneLocation(location);
+    let actual = parseDroneLocation(data);
     assert.deepEqual(actual, expected);
   });
 });

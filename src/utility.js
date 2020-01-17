1540cc8b97b66ca898826dd54f9b263404e77a75
Caleb Ikhuohon <calebikhuohon@gmail.com>
2020-01-17 06:16:50 +0100

    fix: rewrite location data parser to suite current use case

--- src/utility.js
+++ src/utility.js
@@ -3,14 +3,30 @@
  * @param {String} locationBuffer 
  */
 function parseDroneLocation (locationBuffer) {
-    
-}
+  const stringLocation = Buffer.isBuffer(locationBuffer) ? locationBuffer.toString() : locationBuffer;
 
-function verifyCommand(command) {
-  return;
+  const reducer = (finalDataObject, nextDataPoint) => {
+    if (!nextDataPoint.includes(':')) {
+      return finalDataObject;
+    }
+
+    let [key, value] = nextDataPoint.split(':');
+    const isCommaSeperated = value.includes(',');
+
+    if (isCommaSeperated) {
+      value = value.split(',').map(val => (
+        Number.isNaN(parseFloat(val)) ? val : parseFloat(val)
+      ));
+    } else {
+      value = Number.isNaN(parseFloat(value)) ? value : parseFloat(value);
+    }
+
+    return { ...finalDataObject, [key]: value };
+  };
+
+  return stringLocation.split(';').reduce(reducer, {});
 }
 
 export {
   parseDroneLocation,
-  verifyCommand,
 };
